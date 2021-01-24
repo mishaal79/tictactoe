f48309277b33bb0de35d0f57842bf0274987c16a
mishal <mishaal79@gmail.com>
2021-01-24 18:40:22 +1100

    Fix game draw state.

--- main.py
+++ main.py
@@ -11,53 +11,47 @@ def main():
     game = TicTacToe(GRID_SIZE, player1, player2)
     running = True
     game.print_grid()
-    while running:
+    # while running:
+    while (
+        game.state == TicTacToe.STATES.CROSS_TURN
+        or game.state == TicTacToe.STATES.NAUGHT_TURN
+    ):
         if game.state == TicTacToe.STATES.CROSS_TURN:
             print("Player1 turn")
             while True:
-                marker = pydash.map_(input("Input row, column to place marker in: ").split(","), int)
-                if game.place_marker(
-                    CROSS_SYMBOL, marker[0], marker[1]
-                ):
+                marker = pydash.map_(
+                    input("Input row, column to place marker in: ").split(","), int
+                )
+                if game.place_marker(CROSS_SYMBOL, marker[0], marker[1]):
                     game.player1.set_marker(marker)
                     game.player1.process_move()
                     game.state = TicTacToe.STATES.NAUGHT_TURN
                     if game.player1.is_winner():
                         game.state = TicTacToe.STATES.CROSS_WON
+                        print("{} wins the game".format(game.player1.name))
                     break
                 else:
                     print("Please input marker into empty cell")
-
-
-        if game.state == TicTacToe.STATES.NAUGHT_TURN:
+        else:
             print("Player2 turn")
             while True:
-                marker = pydash.map_(input("Input row, column to place marker in: ").split(","), int)
-                if game.place_marker(
-                    NAUGHT_SYMBOL, marker[0], marker[1]
-                ):
+                marker = pydash.map_(
+                    input("Input row, column to place marker in: ").split(","), int
+                )
+                if game.place_marker(NAUGHT_SYMBOL, marker[0], marker[1]):
                     game.player2.set_marker(marker)
                     game.player2.process_move()
                     game.state = TicTacToe.STATES.CROSS_TURN
                     if game.player2.is_winner():
                         game.state = TicTacToe.STATES.NAUGHT_WON
+                        print("{} wins the game".format(game.player2.name))
                     break
                 else:
                     print("Please input marker into empty cell")
 
-        if game.state == TicTacToe.STATES.CROSS_WON:
-            running = False
-            print("{} wins the game".format(game.player1.name))
-
-        if game.state == TicTacToe.STATES.NAUGHT_WON:
-            running = False
-            print("{} wins the game".format(game.player2.name))
-
-        if (
-             game.move_count == GRID_SIZE ^ 2
-        ):
-            running = False
-            print("The game is drawn")
+        if game.move_count == GRID_SIZE ** 2:
+            game.state = TicTacToe.STATES.DRAW
+            print("Game is draw!")
 
 
 if __name__ == "__main__":
